629096606fd220f72438a5d5e3e681d83cd0f71e
Tigran Najaryan <tigran@omnition.io>
2019-11-25 12:20:55 -0500

    Generate Go code
    1. Added makefile to generate code for Go and Java.
    2. Removed no longer needed java_build.sh, now that makefile
       contains all that functionality.
    3. Run `make gen-go` to generate Go code.
    
    This allows generated Go code to be imported and used elsewhere,
    particularly in OpenTelemetry Collector.

--- install-proto.sh
+++ install-proto.sh
@@ -12,3 +12,8 @@ wget https://github.com/grpc-ecosystem/grpc-gateway/releases/download/v1.9.6/pro
 wget https://github.com/protocolbuffers/protobuf/releases/download/v3.9.1/protoc-3.9.1-linux-x86_64.zip \
     && unzip protoc-3.9.1-linux-x86_64.zip \
     && sudo ln -s ${DIR}/bin/protoc /usr/bin/protoc
+
+GIT_TAG="v1.3.2"
+go get -d -u github.com/golang/protobuf/protoc-gen-go
+git -C "$(go env GOPATH)"/src/github.com/golang/protobuf checkout $GIT_TAG
+go install github.com/golang/protobuf/protoc-gen-go
