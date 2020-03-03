609c511da538dc13424ac1f62d2c0f752e3b332b
Nail Islamov <nislamov@google.com>
2020-03-03 12:00:21 +1100

    Print checksum details per file when files are out of sync

--- verify-go.sh
+++ verify-go.sh
@@ -7,6 +7,12 @@ gen_checksum()
     find ./gen -type f -exec md5sum {} \; | sort -k 2 | md5sum
 }
 
+print_checksum_details()
+{
+    find ./gen -type f -exec md5sum {} \; | sort -k 2
+}
+
+print_checksum_details
 CHECKSUM_BEFORE=$(gen_checksum)
 echo "checksum before: ${CHECKSUM_BEFORE}"
 make gen-go
@@ -15,6 +21,7 @@ echo "checksum after: ${CHECKSUM_AFTER}"
 
 if [ "${CHECKSUM_BEFORE}" != "${CHECKSUM_AFTER}" ]
 then
+    print_checksum_details
     echo "'gen' directory is out of sync with protos. Please run 'gen-go.sh' and commit changes."
     exit 1
 fi
