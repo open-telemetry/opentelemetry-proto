e577247486cbdb7b03d6a58e4f829487d1e425d7
jmacd <jmacd@lightstep.com>
2020-07-14 15:42:46 -0700

    Comment gen.go

--- opentelemetry/proto/metrics/v1/gen.go
+++ opentelemetry/proto/metrics/v1/gen.go
@@ -1,4 +1,24 @@
-// go run gen.go > out.go && go run out.go
+// Copyright 2019, OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Notes: This code generates the Kind enum in metrics.proto.  To run this
+// code, execute:
+//
+//   go run gen.go > out.go && go run out.go
+//
+// The output of this program MUST be tested in an external module,
+// using the output of protoc.
 
 package main
 
